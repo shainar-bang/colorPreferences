c9441f8f714367225f6ac80eeef627c5405e0660
shainar-bang <rabangshaina@gmail.com>
2024-11-04 21:20:02 +0800

    Create colorpreferences.js

---
+++ colorpreferences.js
@@ -0,0 +1,13 @@
+function colors() {
+  let favcolors = [];
+
+for (let i = 0; i < 3; i++) {
+    let entercolor = propmt("Enter Your Three Favorite colors.");
+    favcolors.push(enterColor);
+    alert("Your Favorite Colors are: " + favcolors);
+}
+let pushcolor = promt("Add another color you like.");
+favcolors.push(pushcolor);
+alert("Your new colors are: " + favcolors)
+
+}
